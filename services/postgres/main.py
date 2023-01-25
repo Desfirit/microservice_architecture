89c83870f0230f0ba414e853d1e4292b6cd5409d
Rodion Sulokhin <sulohin007@mail.ru>
2023-01-25 18:40:55 +0300

    Added environments

--- services/postgres/main.py
+++ services/postgres/main.py
@@ -1,2 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/api/students", methods=["GET"])
+def get_students():
+    return ["students"]
+
 if __name__ == "__main__":
-    print("Hello world")
+    app.run(host="0.0.0.0", port=os.environ["LOCAL_SERVICES_PORT"])
