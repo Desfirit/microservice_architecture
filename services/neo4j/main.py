b7ebc2859e65a282362b84a708277ded9b93a77e
aleksandr <alexandrl33t@yandex.ru>
2023-01-26 13:24:06 +0300

    s

---
+++ services/neo4j/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, redirect, render_template
+from neo4j import GraphDatabase
+import csv
+
+# uri = "neo4j:22808"
+# driver = GraphDatabase.driver(uri, auth=("neo4j", "neo4j"))
+# session = driver.session()
+# session.run("CREATE OR REPLACE DATABASE graphDb")
+class Neo4jConnection:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        if self.driver is not None:
+            self.driver.close()
+
+    # Метод, который передает запрос в БД
+    def query(self, query, db=None):
+        assert self.driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try:
+            session = self.driver.session(database=db) if db is not None else self.driver.session()
+            response = list(session.run(query))
+        except Exception as e:
+            print("Query failed:", e)
+        finally:
+            if session is not None:
+                session.close()
+        return response
+
+
+conn = Neo4jConnection(uri="neo4j:7687", user="superman", password="pizza")
+conn.query("CREATE OR REPLACE DATABASE graphDb")
+conn.query("SHOW DATABASE")
