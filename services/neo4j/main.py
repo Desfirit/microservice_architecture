6b7020177d68f441f0882381c56cf179a444787d
aleksandr <alexandrl33t@yandex.ru>
2023-01-26 18:28:44 +0300

    sвыф

--- services/neo4j/main.py
+++ services/neo4j/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify, redirect, render_template
-from neo4j import GraphDatabase
+from py2neo import Node, Relationship, Graph, Path, Subgraph
 import urllib.request
-import csv
 import logging
 import sys
 import time
 import os
+import json
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
@@ -14,26 +14,22 @@ logging.getLogger("neo4j").setLevel(logging.DEBUG)
 
 time.sleep(10)
 
+
 # uri = "neo4j:22808"
 # driver = GraphDatabase.driver(uri, auth=("neo4j", "neo4j"))
 # session = driver.session()
 # session.run("CREATE OR REPLACE DATABASE graphDb")
 class Neo4jConnection:
     def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
-
-    def close(self):
-        if self.driver is not None:
-            self.driver.close()
+        self.graph = Graph(uri, auth=(user, password))
 
     # Метод, который передает запрос в БД
     def query(self, query, db=None):
-        assert self.driver is not None, "Driver not initialized!"
+        assert self.graph is not None, "Driver not initialized!"
         session = None
         response = None
         try:
-            session = self.driver.session(database=db) if db is not None else self.driver.session()
-            response = list(session.run(query))
+            response = self.graph.run("CREATE (a:Person {name:{N}})", {"N": "Alice"})
         except Exception as e:
             print("Query failed:", e)
         finally:
@@ -41,14 +37,47 @@ class Neo4jConnection:
                 session.close()
         return response
 
+
+class Student:
+    course_name = None
+    student_id = None
+    group_id = None
+    speciality_id = None
+    departament_name = None
+
+
+class Lesson:
+    lesson_id = None
+    lesson_desctiption = None
+
+
+students = []
 req = urllib.request.Request(f"http://postgres-ma:{os.environ['LOCAL_SERVICES_PORT']}/api/students")
 with urllib.request.urlopen(req) as response:
-   the_page = response.read()
-   print(the_page)
+    the_page = response.read()
+    students_list = json.loads(the_page)
+    for obj in students_list:
+        students.append(
+            {
+                "student_id": obj['id'],
+                "group_id": obj['group_fk'],
+            }
+        )
 
-conn = Neo4jConnection(uri="neo4j:7687", user=os.environ["NEO4J_USER"], password=os.environ["NEO4J_PASS"])
+req = urllib.request.Request(f"http://postgres-ma:{os.environ['LOCAL_SERVICES_PORT']}/api/groups")
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+    group_list = json.loads(the_page)
+    for obj in group_list:
+        for student in students:
+            if student["group_id"] == obj['id']:
+                student["speciality_id"] = obj['speciality_fk']
 
-print("Succesfully connected to neo4j")
-
-conn.query("CREATE OR REPLACE DATABASE graphDb")
-conn.query("SHOW DATABASE")
+print(students)
+# conn = Neo4jConnection(uri="neo4j://localhost:7687", user=os.environ["NEO4J_USER"], password=os.environ["NEO4J_PASS"])
+# cypher_ = "CREATE (n:Student)"
+# graph.run(cypher_)
+# print("Succesfully connected to neo4j")
+#
+# conn.query("CREATE OR REPLACE DATABASE graphDb")
+# conn.query("SHOW DATABASE")
